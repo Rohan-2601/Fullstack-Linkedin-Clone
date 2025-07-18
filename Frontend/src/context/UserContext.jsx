89a7419b383e171614a2146a9b59e4c4fb68b623
Rohan Raj <rjha09307@gmail.com>
2025-07-19 01:29:13 +0530

    Update UserContext.jsx

--- Frontend/src/context/UserContext.jsx
+++ Frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import {  useNavigate } from 'react-router-dom';
 import {io} from "socket.io-client"
 
- export let socket=io("http://localhost:8000")
+ export let socket=io("https://backend-linkedin-clone.onrender.com")
 
 export const userDataContext = createContext();
 
